85a1152e6750348f86f592c8a857c9715dc72f8c
Aykut <ayktshnbs@gmail.com>
2025-07-04 13:45:28 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -26,18 +26,12 @@ const images = [
 ];
 
 const list = document.querySelector(".gallery");
-const fragment = document.createDocumentFragment();
-images.forEach(image => {
-  const item = document.createElement("li");
-  item.classList.add("gallery__item");
 
-  const img = document.createElement("img");
-  img.classList.add("gallery__image");
-  img.src = image.src;
-  img.alt = image.alt;
+const markup = images.map(({ src, alt }) => {
+  return `
+    <li class="gallery__item">
+      <img class="gallery__image" src="${src}" alt="${alt}" />
+    </li>`;
+}).join("");
 
-  item.append(img);
-  fragment.append(item);
-});
-list.append(fragment);
-list.append(fragment);
+list.insertAdjacentHTML("beforeend", markup);
